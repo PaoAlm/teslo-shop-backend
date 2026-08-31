4ed563eb9586b71bcd4e24fc056ec0795cbe96ae
salmonte <salmonte@grainchain.io>
2026-08-31 13:25:36 -0600

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB'),
+        dbName: 'pokemonsdb',
+      }),
+    }),
+    
+    PokemonModule,
+
+    CommonModule,
+
+    SeedModule
+  ],
+})
+
+export class AppModule {}
